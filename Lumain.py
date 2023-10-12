121ab4a24dff54567b387c25aeae6bd709570af8
Helen Lumaine <lumainhelen24@gmail.com>
2023-10-12 20:25:06 +0800

    First Commit

---
+++ Lumain.py
@@ -0,0 +1,12 @@
+#Total amount to pay in a Gasoline Station
+price = 70
+liter = 40
+
+price = int (input("Enter the price: "))
+liter = int (input("Enter the liter: "))
+result = price*liter
+
+print(str(price),"/" + str(liter) + "-" + "" + str(result))
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
